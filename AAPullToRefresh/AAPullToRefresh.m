5496e3c4b7146cd09734aa3b3767b11e30e790ac
speianu <dumitruand2002@gmail.com>
2014-06-04 15:35:25 +0300

    AAPullToRefresh -  code refactoring

--- AAPullToRefresh/AAPullToRefresh.m
+++ AAPullToRefresh/AAPullToRefresh.m
@@ -1,6 +1,16 @@
 #import "AAPullToRefresh.h"
 #import "LYActivityIndicator.h"
 
+#define UIColorFromRGB(rgbValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+#define UIColorFromRGBA(rgbValue, alphaValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:alphaValue]
+
 #define DEGREES_TO_RADIANS(x) (x)/180.0*M_PI
 #define RADIANS_TO_DEGREES(x) (x)/M_PI*180.0
 
